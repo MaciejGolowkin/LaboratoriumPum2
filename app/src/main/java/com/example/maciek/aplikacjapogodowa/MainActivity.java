36bf7e051c750d0e10b2b8b15ce9d22f8fb7551e
Maciej Golowkin <maciek.golowkin@gmail.com>
2017-12-20 00:32:05 +0100

    Zmiana3

--- app/src/main/java/com/example/maciek/aplikacjapogodowa/MainActivity.java
+++ app/src/main/java/com/example/maciek/aplikacjapogodowa/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.maciek.aplikacjapogodowa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button Button_Name=findViewById(R.id.ZmienMiejsce);
+        final EditText Edit_Name=findViewById(R.id.Write_text);
+        final TextView Text_Name=findViewById(R.id.Show_City);
+
+        Button_Name.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Text_Name.setText(Edit_Name.getText().toString());
+            }
+        });
+
     }
 }
