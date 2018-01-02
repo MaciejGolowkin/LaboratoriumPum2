7dac09b0377719498d509e6a8e01bb5cba22e660
Maciej Golowkin <maciek.golowkin@gmail.com>
2018-01-02 21:35:22 +0100

    BindView

--- app/src/main/java/com/example/maciek/aplikacjapogodowa/MainActivity.java
+++ app/src/main/java/com/example/maciek/aplikacjapogodowa/MainActivity.java
@@ -10,35 +10,40 @@ import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.ZmienMiejsce)
+    @BindView(R.id.Zmien_Miejsce)
     Button ZmienMiejsce;
     @BindView(R.id.Write_text)
     EditText Write_text;
+    @BindView(R.id.Show_City)
+    TextView Show_City;
+    @BindView(R.id.Pokaz_Liste)
+    TextView PokazListe;
+    @OnClick(R.id.Zmien_Miejsce)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Button Button_Name=findViewById(R.id.ZmienMiejsce);
-        //final EditText Edit_Name=findViewById(R.id.Write_text);
-        final TextView Text_Name=findViewById(R.id.Show_City);
-        Button Button_show= findViewById(R.id.PokazListe);
         final Intent intent = new Intent(this, Main2Activity.class);
         final Bundle bundle = new Bundle();
         ButterKnife.bind(this);
 
         intent.putExtras(bundle);
 
-        ZmienMiejsce.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Text_Name.setText(Write_text.getText().toString());
-                bundle.putString("klucz" , Text_Name.getText().toString());
-            }
-        });
 
-        Button_show.setOnClickListener(new View.OnClickListener() {
+
+       /* ZmienMiejsce.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Show_City.setText(Write_text.getText().toString());
+                bundle.putString("klucz" , Show_City.getText().toString());
+            }
+        }); */
+
+        PokazListe.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 intent.putExtras(bundle);
