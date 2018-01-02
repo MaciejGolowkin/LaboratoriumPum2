cc37bd628c2f7f6869204032d3bdddd680bb0485
Maciej Golowkin <maciek.golowkin@gmail.com>
2018-01-02 20:38:40 +0100

    zmiany gradle/ mainactivity

--- app/src/main/java/com/example/maciek/aplikacjapogodowa/MainActivity.java
+++ app/src/main/java/com/example/maciek/aplikacjapogodowa/MainActivity.java
@@ -8,24 +8,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.ZmienMiejsce)
+    Button ZmienMiejsce;
+    @BindView(R.id.Write_text)
+    EditText Write_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button Button_Name=findViewById(R.id.ZmienMiejsce);
-        final EditText Edit_Name=findViewById(R.id.Write_text);
+        //Button Button_Name=findViewById(R.id.ZmienMiejsce);
+        //final EditText Edit_Name=findViewById(R.id.Write_text);
         final TextView Text_Name=findViewById(R.id.Show_City);
         Button Button_show= findViewById(R.id.PokazListe);
         final Intent intent = new Intent(this, Main2Activity.class);
         final Bundle bundle = new Bundle();
+        ButterKnife.bind(this);
 
         intent.putExtras(bundle);
 
-        Button_Name.setOnClickListener(new View.OnClickListener() {
+        ZmienMiejsce.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Text_Name.setText(Edit_Name.getText().toString());
+                Text_Name.setText(Write_text.getText().toString());
                 bundle.putString("klucz" , Text_Name.getText().toString());
             }
         });
