26cd99ad605692285e85b55bda3c0be07ec2f1b3
Maciej Golowkin <maciek.golowkin@gmail.com>
2018-01-03 11:26:05 +0100

    Stworzenie konstruktora adapter

--- app/src/main/java/com/example/maciek/aplikacjapogodowa/Main2Activity.java
+++ app/src/main/java/com/example/maciek/aplikacjapogodowa/Main2Activity.java
@@ -5,13 +5,37 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+
 public class Main2Activity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recycler_View;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        RecyclerView.LayoutManager layoutManager= new LinearLayoutManager(getApplicationContext());
-       PlaceAdapter placeAdapter= new PlaceAdapter();
+        RecyclerView.LayoutManager layout_Manager= new LinearLayoutManager(getApplicationContext());
+
+        recycler_View.setLayoutManager(layout_Manager);
+        Place p1= new Place();
+        p1.setPlaceName("Warszawa");
+        p1.setPlaceInformation("Pada śnieg");
+        Place p2= new Place();
+        p2.setPlaceName("Opole");
+        p2.setPlaceInformation("Słonecznie");
+        Place p3= new Place();
+        p3.setPlaceName("Zabrze");
+        p3.setPlaceInformation("Pochmurnie");
+        ArrayList<Place> Places_List= new ArrayList<>();
+        Places_List.add(p1);
+        Places_List.add(p2);
+        Places_List.add(p3);
+        PlaceAdapter placeAdapter= new PlaceAdapter(Places_List);
+        recycler_View.setAdapter(placeAdapter);
     }
 }
--- app/src/main/java/com/example/maciek/aplikacjapogodowa/PlaceAdapter.java
+++ app/src/main/java/com/example/maciek/aplikacjapogodowa/PlaceAdapter.java
@@ -4,11 +4,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 /**
  * Created by Maciek on 03.01.2018.
  */
 
 public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder>{
+
+    ArrayList<Place> Places_List= new ArrayList<>();
+
+    PlaceAdapter(ArrayList<Place> places) {
+        Places_List = places;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
