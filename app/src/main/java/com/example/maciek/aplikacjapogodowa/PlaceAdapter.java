79591f0d3172f6c3aec7c50b38006b686542a366
Maciej Golowkin <maciek.golowkin@gmail.com>
2018-01-03 02:31:31 +0100

    Klasa PlaceAdapter

---
+++ app/src/main/java/com/example/maciek/aplikacjapogodowa/PlaceAdapter.java
@@ -0,0 +1,32 @@
+package com.example.maciek.aplikacjapogodowa;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Maciek on 03.01.2018.
+ */
+
+public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder>{
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
